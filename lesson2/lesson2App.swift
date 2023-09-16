dbdc1c1fdb2f32fb62660ef2100b893dee7b9b27
Yuuto <2021m014@kuas.ac.jp>
2023-09-17 04:58:20 +0900

    Initial Commit

---
+++ lesson2/lesson2App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson2App.swift
+//  lesson2
+//
+//  Created by 千葉勇斗 on 2023/09/17.
+//
+
+import SwiftUI
+
+@main
+struct lesson2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
